31e6e72bed98692f75a343578f0379a3c880584d
Sean Woerner <woerner99@gmail.com>
2021-03-25 20:26:18 -0500

    Resistance now measured correctly. Changed a mistake in pin initialization in measurements.c and also the value that divides WTIMER0_TAV_R

--- measurements.c
+++ measurements.c
@@ -17,13 +17,15 @@
 
 #define BLUE_LED PORTF,2
 #define RED_LED PORTF,1
-#define MEASURE_LR PORTA,3 //PA2
+
+#define MEASURE_LR PORTA,3 //PA3
 #define MEASURE_C PORTE,2 //PE2
-#define ADC PORTE,4 //analog PE4(ADC)
-#define AC PORTC,7 //PC7(AC)
-#define INTEGRATE PORTB,5 //PB5
-#define HIGHSIDE PORTA,2 //PA3
+#define INTEGRATE PORTE,5 //PE5
+#define HIGHSIDE PORTA,2 //PA2
 #define LOWSIDE PORTA,7 //PA7
+
+#define ADC PORTE,4 //analog PE4 ADC
+#define AC PORTC,7 //PC7 AC
 
 
 
@@ -43,20 +45,20 @@ void initMeasure()
     selectPinPushPullOutput(RED_LED);
 
     // Init pins
-    selectPinPushPullOutput(MEASURE_LR); //pa2
-    selectPinPushPullOutput(MEASURE_C); //pe2
+    selectPinPushPullOutput(MEASURE_LR);
+    selectPinPushPullOutput(MEASURE_C);
     selectPinPushPullOutput(INTEGRATE);
     selectPinPushPullOutput(HIGHSIDE);
     selectPinPushPullOutput(LOWSIDE);
 
-    //initalizing the ADC will go here - PE4
+    // Init ADC
     selectPinAnalogInput(ADC);
 
-    //now we have to initialize the Analog Comparator
-    selectPinAnalogInput(AC); //PC7 - AFSEL_R,DEN_R and AMSEL_R
+    // Init AC (Analog Comparator)
+    selectPinAnalogInput(AC);
 
-    //enable a "narrow"/wide timer but it has to be 32-bit
-    // TIMER Configure
+
+    // Config Wide Timer 0
     SYSCTL_RCGCWTIMER_R |= SYSCTL_RCGCWTIMER_R0;                // turn-on timer
     WTIMER0_CTL_R &= ~TIMER_CTL_TAEN;   // turn-off counter before reconfiguring
     WTIMER0_CFG_R = 4;                   // configure as 32-bit counter (A only)
@@ -66,8 +68,8 @@ void initMeasure()
     WTIMER0_TAV_R = 0;                          // zero counter for first period
     WTIMER0_TBV_R = 0;
 
-    //1: enable clocks
-    SYSCTL_RCGCACMP_R |= 1;
+    SYSCTL_RCGCACMP_R |= 1;                     // enable clocks
+
     /* (starts on page 1220)
      5. Configure the internal voltage reference to 1.65 V by writing the ACREFCTL register with the
      value 0x0000.030C.
@@ -75,7 +77,7 @@ void initMeasure()
     COMP_ACREFCTL_R |= 0xF | (1 << 9) | (0 << 8); //(0<<8) RNG=0, (1<<9) = EN, 0xF = 2.469
     COMP_ACCTL0_R |= (2 << 9) | (1 << 1); //ASRCP && (1<<1) CINV we want to invert
 
-    //wait 10us
+    //wait 10us as stated in data sheet
     waitMicrosecond(10);
 
 }
@@ -96,24 +98,22 @@ void groundPins()
 
 uint32_t getResistance()
 {
-    //make function that disables all the pins
-    groundPins();
+    groundPins();                   // ground all pins first
     setPinValue(INTEGRATE, 1);
-    setPinValue(LOWSIDE, 1); //discharge //ground both sides of capacitor
-    waitMicrosecond(10e3); //wait a reasonable time
+    setPinValue(LOWSIDE, 1);        // discharge, ground both sides of capacitor
+    waitMicrosecond(10e5);          // wait a reasonable time
 
-    //disable timer
-    WTIMER0_CTL_R &= ~TIMER_CTL_TAEN;
+    WTIMER0_CTL_R &= ~TIMER_CTL_TAEN;        // disable timer
 
-    //reset timer register TAV and TBV
+    // reset TAV and TBV
     WTIMER0_TAV_R = 0;
     WTIMER0_TBV_R = 0;
 
-    //initiate measure for resistance
+    // Turn on pins to measure resistance
     setPinValue(LOWSIDE, 0);
     setPinValue(MEASURE_LR, 1);
 
-    //turn on timer
+    //  Turn on timer
     WTIMER0_CTL_R |= TIMER_CTL_TAEN;
 
     //stay blocking when it is not tripped - same thing as & with 2
@@ -125,7 +125,7 @@ uint32_t getResistance()
     //disable pins once more before returning
     groundPins();
     //do some math and divide to get accurate resistance and then return value
-    return (WTIMER0_TAV_R/56);
+    return (WTIMER0_TAV_R/57);
 }
 
 
